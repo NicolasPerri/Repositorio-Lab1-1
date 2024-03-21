7cb54e6da741ca34122d7df75f554db583b99b31
Gaspar Nicolas Viola <gaspar5454@hotmail.com>
2024-03-21 15:35:41 -0300

    Agregando un sout

--- Ejercicio de prueba/src/ejercicio/de/prueba/EjercicioDePrueba.java
+++ Ejercicio de prueba/src/ejercicio/de/prueba/EjercicioDePrueba.java
@@ -6,6 +6,7 @@ public class EjercicioDePrueba {
   
     public static void main(String[] args) {
         System.out.println("Hola Gente.");
+        System.out.println("Hola, aqui agregando un sout"); //Agregando un sout
 
     }
     
